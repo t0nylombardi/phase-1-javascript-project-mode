5b27fa9dc0b6d51575757f6c8fbacebbe0be97c4
t0nylombardi <alombardi.331@gmail.com>
2024-04-14 19:09:48 -0400

    started styling wantedCard

--- src/js/index.js
+++ src/js/index.js
@@ -9,11 +9,49 @@ fetch('https://api.fbi.gov/wanted/v1/list')
   .then(data => {
     const items = data.items;
     items.forEach(item => {
-      ({ hair, title, place_of_birth, weight, race, eyes, age_range, details, sex, images, height_max, height_min, weight_min, weight_max, url } = item);
-      console.log(hair, title, place_of_birth, weight);
+      if (item.details != null) renderCard(item);
     });
-    console.log(data);
   })
   .catch(error => console.error(error));
 
 
+const renderCard = (item) => {
+  const {hair, title, place_of_birth, race, eyes, age_range, details, sex, images, height_max, height_min, weight_min, weight_max, url} = item;
+
+  const wantedCard = document.getElementById('wantedCard');
+  const card = document.createElement('div');
+  card.innerHTML = `
+  <div class="card">
+    <h2>${title}</h2>
+    <div id="container">
+      <div class="col1">
+        <div id="wantedCardImages">
+          <div>
+            <img src=${images[0].large} alt=${title} />
+          </div>
+          <div>
+            <img src=${images[1].large} alt=${title} />
+            <p>${images[1].caption}</p>
+          </div>
+        </div>
+      </div>
+      <div class="col2">
+        <p>Age: <span>${isNull(age_range)  }</span></p>
+        <p> Sex: <span>${sex}</span></p>
+        <p> Race: <span>${isNull(race)}</span></p>
+        <p> Height: <span>${isNull(height_min)} lbs - ${isNull(height_max)} lbs</span></p>
+        <p> Weight: <span>${isNull(weight_min)} lbs - ${isNull(weight_max)} lbs</span></p>
+        <p> Eyes: <span>${isNull(eyes)}</span></p>
+        <p> Hair: <span>${isNull(hair)}</span></p>
+        <p> Place of Birth: <span>${isNull(place_of_birth)}</span></p>
+        <div class="card-details">
+          ${details}
+        </div>
+      </div>
+  `
+  wantedCard.appendChild(card);
+}
+
+const isNull = (value) => {
+  return value ? value : 'N/A';
+}
---
+++ src/js/wantedCardTemplate.js
@@ -0,0 +1,10 @@
+
+export const Card = function(person) {
+  return (
+    <div className="card">
+      <img src={this.image[0].large} alt={this.title} />
+      <h2>{wantedCard.name}</h2>
+      <p>{wantedCard.description}</p>
+    </div>
+  );
+}
