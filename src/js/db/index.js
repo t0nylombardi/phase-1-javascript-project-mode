ee970e11542aed1b5d1492ac3b2423eb0481f6bb
t0nylombardi <alombardi.331@gmail.com>
2024-05-01 14:18:51 -0400

    refactored db js to loop through 4 pages of fbi wanted

--- src/js/db/index.js
+++ src/js/db/index.js
@@ -1,6 +1,3 @@
-const currentDate = new Date().toISOString().slice(0, 10); // Get current date in YYYY-MM-DD format
-
-
 /**
  * Fetches data from the specified URL.
  * @param {string} url - The URL to fetch data from.
@@ -26,8 +23,7 @@ const fetchData = async (url) => {
 export const loadDB = async () => {
   try {
     const data = await fetchData('http://localhost:3000/wanted');
-    console.log('Data', data);
-    if (data && data.persons.length === 0) {
+    if (data && data.length === 0) {
       console.log('fetching data from FBI API...');
       await fetchFbiApi();
     }
@@ -42,33 +38,25 @@ export const loadDB = async () => {
  * @returns {Promise<void>} - A Promise that resolves when the data is fetched and the database is populated.
  */
 const fetchFbiApi = async () => {
-  try {
-    const responseData = await fetchData('https://api.fbi.gov/wanted/v1/list');
+  // Check if response data is valid
+  const totalPages = 4; // only looping through 4 pages of data
+  const fetchPromises = [];
 
-    // Check if response data is valid and lastUpdated date is not today
-    if (responseData && responseData.lastUpdated !== currentDate) {
-      const totalPages = 4; // only looping through 4 pages of data
-      const fetchPromises = [];
-
-      const timer = ms => new Promise(res => setTimeout(res, ms)); // Timer function to avoid rate limiting
-      for (let page = 1; page <= totalPages; page++) {
-        const uri = `https://api.fbi.gov/wanted/v1/list?page=${page}`;
-        const data = await fetchData(uri);
-        console.log(`Fetched page ${page}: ${data.items.length} items.`);
-        if (data && data.items) {
-          fetchPromises.push(...data.items.map(PopulateDb));
-        }
-        await timer(1000); // Wait for 1 second to avoid rate limiting  (1000ms = 1s)
+  for (let page = 1; page <= totalPages; page++) {
+    const uri = `https://api.fbi.gov/wanted/v1/list?page=${page}`;
+    await fetchData(uri).then((response) => {
+      return response;
+    }).then((data) => {
+      if (data && data.items) {
+        fetchPromises.push(...data.items);
+        data.items.forEach(PopulateDb);
       }
-
-      await Promise.all(fetchPromises);
-      console.log(`${fetchPromises.length} items added to the database.`);
-    } else {
-      console.log("Data is up to date.");
-    }
-  } catch (error) {
-    console.error(error);
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
   }
+  console.log(`${fetchPromises.length} items added to the database.`);
 };
 
 /**
@@ -77,66 +65,42 @@ const fetchFbiApi = async () => {
  * @returns {Promise<void>} - A Promise that resolves when the person's information is stored in the database.
  */
 const PopulateDb = async (person) => {
-  try {
-    const {
-      age_range,
-      details,
-      description,
-      eyes,
-      hair,
-      height_max,
-      height_min,
-      images,
-      place_of_birth,
-      race,
-      sex,
-      title,
-      url,
-      weight_max,
-      weight_min,
-    } = person;
+  // Construct the request body
+  const requestBody = {
+    age_range: person.age_range,
+    details: person.details,
+    description: person.description,
+    eyes: person.eyes,
+    hair: person.hair,
+    height_max: person.height_max,
+    height_min: person.height_min,
+    images: person.images,
+    place_of_birth: person.place_of_birth,
+    race: person.race,
+    sex: person.sex,
+    title: person.title,
+    url: person.url,
+    weight_max: person.weight_max,
+    weight_min: person.weight_min,
+  };
 
-    // Construct the request body
-    const requestBody = {
-      lastUpdated: currentDate,
-      persons: {
-        age_range,
-        details,
-        description,
-        eyes,
-        hair,
-        height_max,
-        height_min,
-        images,
-        place_of_birth,
-        race,
-        sex,
-        title,
-        url,
-        weight_max,
-        weight_min,
-      } // Populate persons with the provided person object
-    };
+  // Construct the request options
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(requestBody),
+  };
 
-    // Construct the request options
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(requestBody),
-    };
-
-    // Send the request
-    const response = await fetch('http://localhost:3000/wanted', requestOptions);
-
+  // Send the request
+  await fetch('http://localhost:3000/wanted', requestOptions).then((response) => {
     // Check if the response is ok
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
+    if (response.status >= 400 && response.status < 600) {
+      throw new Error("Bad response from server");
     }
-
-    console.log('Data sent successfully:', await response.json());
-  } catch (error) {
+  }).catch((error) => {
+    // Handle errors
     console.error('Error sending data:', error);
-  }
+  });
 };
