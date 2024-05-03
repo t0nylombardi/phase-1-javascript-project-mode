392dd753fe267e6fc7e4856a1e6091f2cbf81b3c
t0nylombardi <alombardi.331@gmail.com>
2024-05-03 11:20:35 -0400

    refactored wantedModal; added Yard Documention

--- src/js/wantedModal.js
+++ src/js/wantedModal.js
@@ -1,18 +1,28 @@
 import { WantedController } from './controllers/wantedController.js';
 import { isNull } from './utils.js';
+
 const Wanted = new WantedController();
 
+/**
+ * Renders a modal to edit details of a wanted person.
+ * @param {Object} person - The wanted person object.
+ */
 export const renderModal = (person) => {
   const modal = document.querySelector('.modal');
   modal.classList.add('open');
   document.body.classList.add('modal-open');
 
-  const modelBody = document.querySelector('.modal-body');
-  modelBody.innerHTML += renderModalHtml(person);
-}
+  const modalBody = document.querySelector('.modal-body');
+  modalBody.innerHTML += createModalContent(person);
+};
 
-const renderModalHtml = (person) => {
-  const detailsObj = [
+/**
+ * Creates the content for the modal.
+ * @param {Object} person - The wanted person object.
+ * @returns {string} The HTML content for the modal.
+ */
+const createModalContent = (person) => {
+  const detailsToRender = [
     'age_range',
     'eyes',
     'hair',
@@ -26,38 +36,79 @@ const renderModalHtml = (person) => {
   return `
     <div class="modal-content">
       <h2 class="title">
-        // Enter new details for <span>${person.title.split('-')[0]}</span>
+        // Enter new details for <span>${getPersonTitle(person)}</span>
       </h2>
-      ${renderForm(person, detailsObj)}
+      ${createForm(person, detailsToRender)}
     </div>
   `;
-}
+};
 
+/**
+ * Gets the title of the person.
+ * @param {Object} person - The wanted person object.
+ * @returns {string} The title of the person.
+ */
+const getPersonTitle = (person) => {
+  return person.title.split('-')[0];
+};
 
-const renderForm = (person, detailsObj) => {
-  const details = Object.keys(person)
-    .filter(key => detailsObj.includes(key))
-    .reduce((obj, key) => {
-      return {...obj, [key]: person[key]};
+/**
+ * Creates the form for editing person details.
+ * @param {Object} person - The wanted person object.
+ * @param {Array} detailsToRender - The details to render in the form.
+ * @returns {string} The HTML content for the form.
+ */
+const createForm = (person, detailsToRender) => {
+  const details = filterDetails(person, detailsToRender);
+
+  return `
+    <form class="person-details-form">
+      ${renderFormElements(details)}
+      <button class="wanted-btn" type="submit">Submit</button>
+    </form>
+  `;
+};
+
+/**
+ * Filters the details object based on provided keys.
+ * @param {Object} person - The wanted person object.
+ * @param {Array} detailsToRender - The details keys to render.
+ * @returns {Object} The filtered details object.
+ */
+const filterDetails = (person, detailsToRender) => {
+  return Object.keys(person)
+    .filter(key => detailsToRender.includes(key))
+    .reduce((filteredDetails, key) => {
+      return { ...filteredDetails, [key]: person[key] };
     }, {});
+};
 
-    return `
-      <form class="person-details-form">
-        ${Object.keys(details).map(key => `
-          <div class="variables form-group">
-            <span class="variable-type">let</span>
-            <span class="variable-name">${key.camelCase()}</span>
-            <span class="variable-assignment">=</span>
-            <input
-              type="text"
-              class="variable-value"
-              id="${key}"
-              name="${key}"
-              value="${isNull(details[key])}">
-          </div>
-        `).join('')}
-        <button class="wanted-btn" type="submit">Submit</button>
-      </form>
-    `;
-}
+/**
+ * Renders form elements based on details object.
+ * @param {Object} details - The details object.
+ * @returns {string} The HTML content for form elements.
+ */
+const renderFormElements = (details) => {
+  return Object.keys(details).map(key => `
+    <div class="variables form-group">
+      <span class="variable-type">let</span>
+      <span class="variable-name">${toCamelCase(key)}</span>
+      <span class="variable-assignment">=</span>
+      <input
+        type="text"
+        class="variable-value"
+        id="${key}"
+        name="${key}"
+        value="${isNull(details[key])}">
+    </div>
+  `).join('');
+};
 
+/**
+ * Converts a string to camel case.
+ * @param {string} str - The string to convert.
+ * @returns {string} The string in camel case.
+ */
+const toCamelCase = (str) => {
+  return str.camelCase();
+};
