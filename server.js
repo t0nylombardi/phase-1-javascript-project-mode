801a51f273d4acb61476936a96d24e77fa032b3e
t0nylombardi <alombardi.331@gmail.com>
2024-06-13 20:13:25 -0400

    edit package.json

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 // Description: This file is used to run the JSON server on Heroku.
-import jsonServer from "json-server";
-// jsonServer = require("json-server");
+// import jsonServer from "json-server";
+jsonServer = require("json-server");
 
 const server = jsonServer.create();
 const router = jsonServer.router("src/db.json");
 const middlewares = jsonServer.defaults({ static: "./build" });
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 server.use(middlewares);
 server.use(router);
-console.log(`JSON Server is running on port ${port}`);
-server.listen(port);
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
